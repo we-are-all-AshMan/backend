0cf3158c188e441f7b73f057e9c6e615e8065416
Zyh2333 <1245231959@qq.com>
2020-07-08 16:23:28 +0800

    搭建后端框架，包命名

---
+++ src/main/java/cn/edu/whu/ashman/AIGOVMainApplication.java
@@ -0,0 +1,16 @@
+package cn.edu.whu.ashman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zhuyuhan
+ * @date 2020-07-08 16:10
+ */
+
+@SpringBootApplication
+public class AIGOVMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AIGOVMainApplication.class,args);
+    }
+}
