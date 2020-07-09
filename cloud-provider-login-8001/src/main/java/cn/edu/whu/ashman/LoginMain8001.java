5c15a2fd76fec8bbf3aa2b2e417f4c7d64ff0b2c
Zyh2333 <1245231959@qq.com>
2020-07-09 10:09:09 +0800

    重构后端框架

--- cloud-provider-login-8001/src/main/java/cn/edu/whu/ashman/LoginMain8001.java
+++ cloud-provider-login-8001/src/main/java/cn/edu/whu/ashman/LoginMain8001.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Zhuyuhan
- * @date 2020-07-08 16:10
+ * @date 2020-07-09 0:06
  */
-
 @SpringBootApplication
-public class AIGOVMainApplication {
+public class LoginMain8001 {
     public static void main(String[] args) {
-        SpringApplication.run(AIGOVMainApplication.class,args);
+        SpringApplication.run(LoginMain8001.class,args);
     }
 }
