0ae245183c4ae3cd7f58ec74794572783db27921
维帆 刘 <1798707219@qq.com>
2020-07-15 21:56:36 +0800

    机器人问答（身份相关），反馈热门问题，演示可用

--- cloud-provider-aiRobot-8004/src/main/java/cn/edu/whu/ashman/service/Impl/QuestionService.java
+++ cloud-provider-aiRobot-8004/src/main/java/cn/edu/whu/ashman/service/Impl/QuestionService.java
@@ -25,12 +25,13 @@ public class QuestionService implements IQuestionService {
 
     /**
      * Question数组转String数组
+     *
      * @param questionList
      * @return
      */
-    private List<String> QuestionsToString(List<Question> questionList){
+    private List<String> QuestionsToString(List<Question> questionList) {
         List<String> result = new ArrayList<String>();
-        for (Question q:questionList) {
+        for (Question q : questionList) {
             result.add(q.getQuestion());
         }
         return result;
@@ -38,94 +39,124 @@ public class QuestionService implements IQuestionService {
 
     /**
      * 获得热门的话题
-     * @return
-     */
-    public List<String> getHotQuestion(){
-        questions = selectAllQuestions();
-        Collections.sort(questions);
-        List<String> result = new ArrayList<String>();
-        if(questions.size() < 6){
-            return QuestionsToString(questions);
-        }else {
-            return QuestionsToString(questions.subList(0,4));
-        }
+     * @return public List<String> getHotQuestion(){
+    questions = selectAllQuestions();
+    Collections.sort(questions);
+    List<String> result = new ArrayList<String>();
+    if(questions.size() < 6){
+    return QuestionsToString(questions);
+    }else {
+    return QuestionsToString(questions.subList(0,4));
     }
-
-    /**
-     * 读取question.txt文件
-     * @param fileName
-     * @return
-
-    private File getFile(String fileName) {
-        ClassLoader classLoader = getClass().getClassLoader();
-
-         getResource()方法会去classpath下找这个文件，获取到url resource, 得到这个资源后，调用url.getFile获取到 文件 的绝对路径
-
-        URL url = classLoader.getResource(fileName);
-
-          url.getFile() 得到这个文件的绝对路径
-
-        System.out.println(url.getFile());
-        File file = new File(url.getFile());
-        return file;
     }*/
-
     /**
-     * 管理端添加问题
-     * @param question
+     * 获得热门话题，演示用
+     *
+     * @return
      * @throws Exception
      */
-    public void insertQuestionForAdministrator(String question) throws Exception{
-        if(questions.contains(question)){
-            throw new Exception("问题库已包含该问题");
-        }
-        char[] questionMessage = question.toCharArray();
-        /**
-        //向文本中添加问题
-        File file = getFile("question.txt");
-        System.out.println(file.getAbsolutePath());
-        OutputStream out = new FileOutputStream(file);
-        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(out));
-        br.newLine();
-        for(char s:questionMessage) {
-         br.append(s);
-        }*/
-        insertQuestion(new Question(question,0));
-    }
-
-    /**
-     * 管理端删除问题
-     * @return
-     */
-    public void deleteQuestionForAdministrator(String question) throws Exception{
-        if(!questions.contains(question)){
-            throw new Exception("问题库无该问题");
-        }
-        /**
+    public List<String> getHotQuestion() throws Exception {
         File file = getFile("question.txt");
         System.out.println(file.getAbsolutePath());
         InputStream in = new FileInputStream(file);
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        br.newLine();
-        for(char s:questionMessage) {
-            br.append(s);*/
-        deleteQuestion(new Question(question,0));
+        List<String> questionList = new ArrayList<String>();
+        String line = null;
+        for (int i = 0; i < 5;i++){
+            line = br.readLine();
+            questionList.add(line);
+        }
+        return questionList;
     }
 
-    public void updateAnswer(Question question) throws Exception{
-        if(!questions.contains(question)){
+    /**
+     * 读取question.txt文件
+     *
+     * @param fileName
+     * @return
+     */
+    private File getFile(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        // getResource()//方法会去classpath下找这个文件，获取到url resource, 得到这个资源后，调用url.getFile获取到 文件 的绝对路径
+
+        URL url = classLoader.getResource(fileName);
+
+        // url.getFile(); //得到这个文件的绝对路径
+
+        System.out.println(url.getFile());
+        File file = new File(url.getFile());
+        return file;
+    }
+
+    /**
+     * 管理端添加问题
+     *
+     * @param question
+     * @throws Exception
+     */
+    public void insertQuestionForAdministrator(String question) throws Exception {
+        if (questions.contains(question)) {
+            throw new Exception("问题库已包含该问题");
+        }
+        char[] questionMessage = question.toCharArray();
+        /**
+         //向文本中添加问题
+         File file = getFile("question.txt");
+         System.out.println(file.getAbsolutePath());
+         OutputStream out = new FileOutputStream(file);
+         BufferedWriter br = new BufferedWriter(new OutputStreamWriter(out));
+         br.newLine();
+         for(char s:questionMessage) {
+         br.append(s);
+         }*/
+        insertQuestion(new Question(question, 0));
+    }
+
+    /**
+     * 管理端删除问题
+     *
+     * @return
+     */
+    public void deleteQuestionForAdministrator(String question) throws Exception {
+        if (!questions.contains(question)) {
+            throw new Exception("问题库无该问题");
+        }
+        /**
+         File file = getFile("question.txt");
+         System.out.println(file.getAbsolutePath());
+         InputStream in = new FileInputStream(file);
+         BufferedReader br = new BufferedReader(new InputStreamReader(in));
+         br.newLine();
+         for(char s:questionMessage) {
+         br.append(s);*/
+        deleteQuestion(new Question(question, 0));
+    }
+
+    public void updateAnswer(Question question) throws Exception {
+        if (!questions.contains(question)) {
             throw new Exception("问题库无该问题");
         }
         questionDao.updateAnswer(question);
     }
 
-    List<Question> selectAllQuestions(){return questionDao.selectAllQuestions();}
+    List<Question> selectAllQuestions() {
+        return questionDao.selectAllQuestions();
+    }
 
-    public void insertQuestion(Question question){questionDao.insertQuestion(question);}
+    public void insertQuestion(Question question) {
+        questionDao.insertQuestion(question);
+    }
 
-    public void deleteQuestion(Question question){questionDao.deleteQuestion(question);}
+    public void deleteQuestion(Question question) {
+        questionDao.deleteQuestion(question);
+    }
 
-    public void updateQuestion(Question question){questionDao.updateSetAskTimeIsZero();}
+    public void updateQuestion(Question question) {
+        questionDao.updateSetAskTimeIsZero();
+    }
 
-    public void updateQuestionPlusOne(String question){questionDao.updateSetAskTimePlusOne(question);}
+    public void updateQuestionPlusOne(String question) {
+        questionDao.updateSetAskTimePlusOne(question);
+    }
 }
